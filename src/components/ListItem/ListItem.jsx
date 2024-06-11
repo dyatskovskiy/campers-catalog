1eb31334b1e53e45d96d730d7c848d83a4f02773
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-06-11 22:56:43 +0300

    styling. Stopped on svg

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,8 +1,21 @@
+import css from './LIstItem.module.css';
+
 export const CamperCard = ({ camperData }) => {
-  console.log(camperData);
   return (
-    <div>
-      <p>{camperData.name}</p>
+    <div className={css.wrapper}>
+      <img className={css.camperPhoto} src={camperData.gallery[0]} alt="Camper" />
+
+      <div className={css.mainInfoBlock}>
+        <div className={css.topTextBlock}>
+          <h3 className={css.topText}>{camperData.name}</h3>
+
+          <div className={css.priceBlock}>
+            <p className={css.topText}>{`€${camperData.price}.00`}</p>
+
+            <button className={css.favoriteButton} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
